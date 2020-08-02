76a5886779ecce1a619fc6336b3721ac97436a91
neiljaitly7963 <neiljaitly7963@gmail.com>
2020-08-03 00:13:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SpotifyWebApi from 'spotify-web-api-js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const spotifyApi = new SpotifyWebApi();
+
+class App extends React.Component {
+
+  constructor () {
+    super()
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: 'Not Checked', albumArt: '' }
+    }
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+        hashParams[e[1]] = decodeURIComponent(e[2]);
+        e = r.exec(q)
+    }
+    return hashParams;
+  }
+
+
+
+  getNowPlaying(){
+  spotifyApi.getMyCurrentPlaybackState()
+    .then((response) => {
+      this.setState({
+        nowPlaying: { 
+            name: response.item.name, 
+            albumArt: response.item.album.images[0].url
+          }
+      });
+    })
+}
+
+getUserPlaylist(){
+  spotifyApi.getUserPlaylists().then(response => {
+    console.log(response)
+  })
+}
+
+  render() {
+      return (
+        <div className="App">
+          <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {
+            this.state.loggedIn ? 
+            <div>
+              <h1>welcome</h1>
+              <button onClick={() => {
+                this.getNowPlaying()
+                this.getUserPlaylist()
+              }} >click me</button>
+              <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} />
+              <h1>{this.state.nowPlaying.name}</h1>
+            </div>
+            :
+            <a className="App-link" href="http://localhost:8888"> Login to Spotify </a>
+ 
+          }
+          </header>
+        </div>
   );
+  }
 }
 
 export default App;
+
+
